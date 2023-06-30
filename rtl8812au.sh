b30cf1770427819a56fa42ceef22ed9939c4d637
Ashish <deshpandeashish90@gmail.com>
2023-06-30 20:49:07 +0530

    Updated rtl8812au.sh

--- rtl8812au.sh
+++ rtl8812au.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S make dkms paru --noconfirm 
+#sudo pacman -S make dkms paru --noconfirm 
 
 #if connected to internet then clone this repo
 #git clone https://github.com/aircrack-ng/rtl8812au.git
@@ -11,6 +11,19 @@ unzip rtl8812au-5.6.4.2.zip
 cd rtl8812au-5.6.4.2/
 
 sudo make dkms_install
-sleep 4
+
+echo "WIFI Driver installed onto your system please reboot"
+
+read -p "Enter (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+	echo "Rebooting Now"
+	sleep 3
+	reboot
+    ;;
+    * )
+        echo exit
+    ;;
+esac
 
 
