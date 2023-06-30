ce48d6051dea5fa1baecb445ba0624523c1dd0d6
Ashish <deshpandeashish90@gmail.com>
2023-06-30 14:55:29 +0530

    Updated rtl8812au.sh

--- rtl8812au.sh
+++ rtl8812au.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 sudo pacman -S make dkms paru --noconfirm 
-git clone https://github.com/aircrack-ng/rtl8812au.git
 
-cd rtl8812au
+#if connected to internet then clone this repo
+#git clone https://github.com/aircrack-ng/rtl8812au.git
+#cd rtl8812au
+
+#if no internet access
+unzip rtl8812au-5.6.4.2.zip 
+cd rtl8812au-5.6.4.2/
+
 sudo make dkms_install
-sleep 7
-reboot
+sleep 4
+
+
