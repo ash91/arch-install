a36fd701da72a7107a502ccebd453ecbf5526c01
ashish <deshpandeashish90@gmail.com>
2023-07-25 21:21:28 +0530

    updated themes.sh

--- themes.sh
+++ themes.sh
@@ -14,7 +14,7 @@ case $distro in
     1) yay -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter --noconfirm && sudo systemctl enable lightdm;;
     2) yay -S gnome gnome-extra gnome-themes-extra gnome-tweaks gtk-engine-murrine --noconfirm && sudo systemctl enable gdm;;
     3) yay -S plasma --noconfirm && sudo systemctl enable sddm;;
-    4) yay -S cinnamon --noconfirm && sudo systemctl enable lightdm;;
+    4) yay -S cinnamon lightdm lightdm-gtk-greeter --noconfirm && sudo systemctl enable lightdm;;
     *) echo "Please enter valid choice."
 esac
 #Download plank themes
