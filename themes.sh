afaae8c222258fa66313169628b9fe0a43f4f3aa
ashish <deshpandeashish90@gmail.com>
2023-08-10 12:32:36 +0530

    updated themes.sh

--- themes.sh
+++ themes.sh
@@ -12,11 +12,11 @@ echo -n "Enter Desktop Environment of your choice: "
 read distro;
 
 case $distro in
-    1) yay -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter --noconfirm; sudo systemctl enable --now lightdm;;
-    2) yay -S gnome gnome-extra gnome-themes-extra gnome-tweaks gtk-engine-murrine --noconfirm; sudo systemctl enable --now gdm;;
-    3) yay -S plasma --noconfirm; sudo systemctl enable --now sddm;;
-    4) yay -S cinnamon lightdm lightdm-gtk-greeter --noconfirm; sudo systemctl enable --now lightdm;;
-    5) yay -S qtile && sudo systemctl enable --now lightdm;;
+    1) yay -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter --noconfirm; sudo systemctl enable lightdm;;
+    2) yay -S gnome gnome-extra gnome-themes-extra gnome-tweaks gtk-engine-murrine --noconfirm; sudo systemctl enable gdm;;
+    3) yay -S plasma --noconfirm; sudo systemctl enable sddm;;
+    4) yay -S cinnamon lightdm lightdm-gtk-greeter --noconfirm; sudo systemctl enable lightdm;;
+    5) yay -S qtile && sudo systemctl enable lightdm;;
     *) echo "Please enter valid choice."
 esac
 #Download plank themes
